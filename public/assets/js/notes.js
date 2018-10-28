fbc3339cb26025afe6d07e5d536f7d3b358c43fc
Jose M. Vasconcellos <miguel_vasconce@hotmail.com>
2018-10-28 19:30:08 -0400

    NodeJS Note Taker App

--- public/assets/js/notes.js
+++ public/assets/js/notes.js
@@ -86,6 +86,7 @@ $(document).ready(function(){
                 $('#note-text').val('');
               });
               newNote = true;
+              newNoteBtn.css("display","none");
           });
           /***************************************/
         });
